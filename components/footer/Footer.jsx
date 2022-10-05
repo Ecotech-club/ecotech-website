552012049ee77b7619657ad7b09df4ecaaa686d2
Khawla BOKHTACHE <khawla.bokhtache@etu.usthb.dz>
2022-10-05 21:06:49 +0200

    finished tasks

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { FaFacebookSquare, FaInstagramSquare , FaLinkedin,FaGooglePlusSquare  } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className= 'bg-gradient-to-b from-secondary/20 to-main   p-14'>
+    <div className=''>
+    <div className='flex justify-center'>
+    <div className=' bg-main h-1.5 w-20 min-w-min	 ml-2 rounded-3xl '></div>
+     <h1 className='font-sans   text-sm lg:text-4xl   xs:text-2xl font-bold relative  sm:text-3xl  md:text-4xl text-main  not-italic  -top-5 mx-3'>Suivez Nous</h1>
+     <div className=' bg-main h-1.5 w-20 min-w-min	 ml-2 rounded-3xl '></div>
+    </div>
+     <ul>
+       <div className='flex gap-6 pb-5 justify-center items-cerntre m-8'>
+         {/* Sectin for icons  */}
+         <a href="https://web.facebook.com/WhereTechnologyMeetsEcology?_rdc=1&_rdr" target="_blank" rel="noopener noreferrer"><FaFacebookSquare className='text-4xl text-main cursor-pointer hover:text-secondary ' /></a>
+         <a href="https://www.instagram.com/ecotech.club/" target="_blank" rel="noopener noreferrer"><FaInstagramSquare className='text-4xl text-main  cursor-pointer hover:text-secondary' /></a>
+         <a href="https://www.linkedin.com/company/ecotech-esi/" target="_blank" rel="noopener noreferrer"><FaLinkedin className='text-4xl text-main  cursor-pointer hover:text-secondary' /></a>
+         <a href="https://mail.google.com/mail/u/0/?hl=fr&tf=cm&fs=1&to=ecotech@esi.dz" target="_blank" rel="noopener noreferrer"><FaGooglePlusSquare className='text-4xl text-main  cursor-pointer hover:text-secondary' /></a>
+       </div>
+     </ul>
+     <div className='flex justify-center'>
+     <p className='font-sans text-white text-xs'>Copyright © 2022 Club Ecotech de l'ESI. All Rights Reserved.</p>
+    </div>
+    </div>
+    </footer>
   )
 }
 
--- components/support/Support.jsx
+++ components/support/Support.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Support = () => {
   return (
-    <div>Support</div>
+    <div className='lg:w-1024 p-20 bg-gradient-to-b from-secondary/20 to-main w-full h-full  '>
+    <div className='items-center flex'>
+       <div className=' bg-main h-1.5 w-1/6 min-w-min	 ml-2 rounded-3xl mb-3 '></div>
+       <div className='bg-main h-4 w-4 relative -left-1  rounded-3xl mb-3 xs:w-3 xs:h-3 lg:h-4 lg:w-4  min-w-min'></div>
+       <div className='font-sans sm:text-3xl  md:text-4xl m-4  text-main  not-italic font-bold relative -top-1 '><h1>Nous Soutenir</h1></div>
+    </div>
+    <div className='py-16 px-3'>
+        <p className='font-sans text-white lg:text-3xl sm:text-2xl leading-7 md:leading-8 tracking-wider mb-2 '> Si vous êtes un sponsor et  vous souhaitez soutenir notre club pour avoir plus de succès et des impacts, vous n'avez qu'à cliquer sur ce bouton ! </p>
+        <div className='w-full flex  items-center mt-6 '>
+        <button className='lg:w-60 lg:h-15 text-centre sm:h-14 sm:w-40 w-40 h-12  rounded-[40px] text-white lg:text-2xl text-lg font-sans transition duration-200 ease-in transform sm:hover:scale-105 bg-main tracking-wider leading-8 my-8 py-3  md:py-2 shadow-md'>Nous Soutenir</button>
+        </div>
+    </div>
+   </div>
   )
 }
 
